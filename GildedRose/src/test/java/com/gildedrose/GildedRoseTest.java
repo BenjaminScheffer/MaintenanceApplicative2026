3684919ac88bbc3d488c431e3418bbc669ca5a8b
BenjaminScheffer <benjamin.scheffer7@etu.univ-lorraine.fr>
2026-03-11 17:04:10 +0100

    Coverage 100 % des branch

--- GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -11,7 +11,92 @@ class GildedRoseTest {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
     }
 
+    @Test
+    void test1() {
+        Item[] items = new Item[] { new Item("foo", 0, 1) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    void test2() {
+        Item[] items = new Item[] { new Item("Aged Brie", 2, 1) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(2, app.items[0].quality);
+    }
+    @Test
+    void test3() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 2, 1) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(4, app.items[0].quality);
+    }
+    @Test
+    void test4() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 2, 1) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(1, app.items[0].quality);
+    }
+
+    @Test
+    void test5() {
+        Item[] items = new Item[] { new Item("Aged Brie", 2, 52) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(52, app.items[0].quality);
+    }
+
+    @Test
+    void test6() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 12, 49) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(50, app.items[0].quality);
+    }
+
+    @Test
+    void test7() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", -12, 49) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    void test8() {
+        Item[] items = new Item[] { new Item("AAA", -12, 30) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(28, app.items[0].quality);
+    }
+
+    @Test
+    void test9() {
+        Item[] items = new Item[] { new Item("Aged Brie", -12, 30) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(32, app.items[0].quality);
+    }
+
+    @Test
+    void test10() {
+        Item[] items = new Item[] { new Item("Aged Brie", -12, 550) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(550, app.items[0].quality);
+    }
+
+    @Test
+    void test11() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", -12, 550) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(550, app.items[0].quality);
+    }
 }
