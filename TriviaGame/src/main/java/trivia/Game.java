50de5c231224a9afa116dfc4ec90db5e694d6ff1
BenjaminScheffer <benjamin.scheffer7@etu.univ-lorraine.fr>
2026-03-16 09:28:32 +0100

    use player in game

--- TriviaGame/src/main/java/trivia/Game.java
+++ TriviaGame/src/main/java/trivia/Game.java
@@ -5,117 +5,100 @@ import printer.Printer;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
-// REFACTOR ME
 public class Game implements IGame {
    public static final int NB_QUESTION = 50;
-
    public static final String POP = "Pop";
    public static final String SCIENCE = "Science";
    public static final String SPORTS = "Sports";
    public static final String ROCK = "Rock";
    public static final int BOARD_SIZE = 12;
 
-   ArrayList<Player> players = new ArrayList<Player>();
-   int[] places = new int[6];
-   int[] purses = new int[6];
-   boolean[] inPenaltyBox = new boolean[6];
+   private final List<Player> players = new ArrayList<>();
+   private final LinkedList<String> popQuestions = new LinkedList<>();
+   private final LinkedList<String> scienceQuestions = new LinkedList<>();
+   private final LinkedList<String> sportsQuestions = new LinkedList<>();
+   private final LinkedList<String> rockQuestions = new LinkedList<>();
 
-   LinkedList popQuestions = new LinkedList();
-   LinkedList scienceQuestions = new LinkedList();
-   LinkedList sportsQuestions = new LinkedList();
-   LinkedList rockQuestions = new LinkedList();
-
-   int currentPlayer = 0;
-   boolean isGettingOutOfPenaltyBox;
-
-   Printer printer = new ConsolePrinter();
+   private int currentPlayerIndex = 0;
+   private boolean isGettingOutOfPenaltyBox;
+   private final Printer printer = new ConsolePrinter();
 
    public Game() {
-      initQuestion();
+      initQuestions();
    }
 
-   public void initQuestion(){
+   private void initQuestions() {
       for (int i = 0; i < NB_QUESTION; i++) {
          popQuestions.addLast("Pop Question " + i);
          scienceQuestions.addLast("Science Question " + i);
          sportsQuestions.addLast("Sports Question " + i);
-         rockQuestions.addLast( "Rock Question " + i);
+         rockQuestions.addLast("Rock Question " + i);
       }
    }
 
    public boolean add(String playerName) {
-      places[howManyPlayers()] = 1;
-      purses[howManyPlayers()] = 0;
-      inPenaltyBox[howManyPlayers()] = false;
       players.add(new Player(playerName));
-
       printer.print(playerName + " was added");
       printer.print("They are player number " + players.size());
       return true;
    }
 
-   public int howManyPlayers() {
-      return players.size();
+   private Player getCurrentPlayer() {
+      return players.get(currentPlayerIndex);
    }
 
    public void roll(int roll) {
-      printer.print(players.get(currentPlayer) + " is the current player");
+      Player player = getCurrentPlayer();
+      printer.print(player + " is the current player");
       printer.print("They have rolled a " + roll);
 
-      if (inPenaltyBox[currentPlayer]) {
-         handlePenaltyBox(roll);
-         return;
-      }
-
-      movePlayer(roll);
-      askCurrentQuestion();
-
-   }
-   private void handlePenaltyBox(int roll) {
-      if (roll % 2 != 0) {
-         isGettingOutOfPenaltyBox = true;
-         printer.print(players.get(currentPlayer) + " is getting out of the penalty box");
-         movePlayer(roll);
-         askCurrentQuestion();
+      if (player.isInPenaltyBox()) {
+         if (roll % 2 != 0) {
+            isGettingOutOfPenaltyBox = true;
+            printer.print(player + " is getting out of the penalty box");
+            movePlayerAndAsk(player, roll);
+         } else {
+            printer.print(player + " is not getting out of the penalty box");
+            isGettingOutOfPenaltyBox = false;
+         }
       } else {
-         printer.print(players.get(currentPlayer) + " is not getting out of the penalty box");
-         isGettingOutOfPenaltyBox = false;
+         movePlayerAndAsk(player, roll);
       }
    }
 
-   public void askCurrentQuestion(){
+   private void movePlayerAndAsk(Player player, int roll) {
+      player.move(roll, BOARD_SIZE);
+      printer.print(player + "'s new location is " + player.getPlace());
+      askCurrentQuestion();
+   }
+
+   public void askCurrentQuestion() {
       printer.print("The category is " + currentCategory());
       askQuestion();
-   }
-
-   public void movePlayer(int roll){
-      places[currentPlayer] = ((places[currentPlayer] + roll - 1) % BOARD_SIZE) + 1;
-      printer.print(players.get(currentPlayer)
-              + "'s new location is "
-              + places[currentPlayer]);
    }
 
    private void askQuestion() {
       switch(currentCategory()) {
          case POP:
-            printer.print(popQuestions.removeFirst().toString());
+            printer.print(popQuestions.removeFirst());
             break;
          case SCIENCE:
-            printer.print(scienceQuestions.removeFirst().toString());
+            printer.print(scienceQuestions.removeFirst());
             break;
          case SPORTS:
-            printer.print(sportsQuestions.removeFirst().toString());
+            printer.print(sportsQuestions.removeFirst());
             break;
          default:
-            printer.print(rockQuestions.removeFirst().toString());
+            printer.print(rockQuestions.removeFirst());
             break;
       }
    }
 
 
    private String currentCategory() {
-      int position = (places[currentPlayer] - 1) % 4;
+      int position = (getCurrentPlayer().getPlace() - 1) % 4;
       switch (position) {
          case 0: return POP;
          case 1: return SCIENCE;
@@ -125,18 +108,16 @@ public class Game implements IGame {
    }
 
    public boolean handleCorrectAnswer() {
+      Player player = getCurrentPlayer();
 
-      if (inPenaltyBox[currentPlayer] && !isGettingOutOfPenaltyBox) {
+      if (player.isInPenaltyBox() && !isGettingOutOfPenaltyBox) {
          nextPlayer();
          return true;
       }
 
       printer.print("Answer was correct!!!!");
-      purses[currentPlayer]++;
-      printer.print(players.get(currentPlayer)
-              + " now has "
-              + purses[currentPlayer]
-              + " Gold Coins.");
+      player.addGold();
+      printer.print(player + " now has " + player.getPurse() + " Gold Coins.");
 
       boolean winner = didPlayerWin();
       nextPlayer();
@@ -144,24 +125,22 @@ public class Game implements IGame {
       return winner;
    }
 
-   private void nextPlayer() {
-      currentPlayer++;
-      if (currentPlayer == players.size()) {
-         currentPlayer = 0;
-      }
-   }
-
    public boolean wrongAnswer() {
+      Player player = getCurrentPlayer();
       printer.print("Question was incorrectly answered");
-      printer.print(players.get(currentPlayer) + " was sent to the penalty box");
-      inPenaltyBox[currentPlayer] = true;
+      printer.print(player + " was sent to the penalty box");
+      player.setInPenaltyBox(true);
 
       nextPlayer();
       return true;
    }
 
+   private void nextPlayer() {
+      currentPlayerIndex = (currentPlayerIndex + 1) % players.size();
+   }
 
    private boolean didPlayerWin() {
-      return !(purses[currentPlayer] == 6);
+      return !(getCurrentPlayer().getPurse() == 6);
    }
+
 }
